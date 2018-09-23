eb43c38b783e0ad6fe81742868fc3ff768409573
Ash Matheson <nuclearfossil@hotmail.com>
2018-09-22 21:38:34 -0700

    Premake5 to build

---
+++ build/generate.bat
@@ -0,0 +1,3 @@
+@pushd
+..\bin\premake5.exe vs2017
+@popd
---
+++ build/premake5.lua
@@ -0,0 +1,45 @@
+-- premake5.lua
+
+workspace "FbxDebugger"
+    configurations { "Debug", "Release"}
+    location "sln"
+
+project "Viewer"
+    location "sln/viewer"
+    kind "ConsoleApp"    
+    language "C++"
+    system "windows"
+    targetdir "targets/%{cfg.buildcfg}"
+
+    local srcPath = "../src/"
+    local imguiPath = "../extern/imgui/"
+    local glfwPath = "../extern/glfw/"
+    local glewPath = "../extern/glew/"
+    files {
+        imguiPath .. "*.h", imguiPath .. "*.cpp",
+        srcPath .. "**.h", srcPath .. "**.cpp"
+    }
+
+    includedirs {
+        imguiPath,
+        srcPath,
+        glfwPath .. "include",
+        glewPath .. "include"
+    }
+
+    libdirs { 
+        glfwPath .. "lib/static/v140/x64",
+        glewPath .. "lib/v110/x64/Release/static"
+ }
+
+    filter "configurations:Debug"
+        architecture "x86_64"
+        links {"d3d11.lib", "d3dcompiler.lib", "dxgi.lib", "opengl32.lib", "glfw3.lib", "glew.lib"}
+        defines {"DEBUG"}
+        symbols "On"
+
+    filter "configurations:Release"
+        architecture "x86_64"
+        links {"d3d11.lib", "d3dcompiler.lib", "dxgi.lib", "opengl32.lib", "glfw3.lib", "glew.lib"}
+        defines {"NDEBUG"}
+        optimize "On"
